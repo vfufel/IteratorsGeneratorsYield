95bfd8d76952cadc0256e95ed61468f3e027304f
vfufel <bubelev.vlad@gmail.com>
2024-08-08 14:59:10 +0300

    Fixed indentation in the first task

--- 1_FlatIterator.py
+++ 1_FlatIterator.py
@@ -22,22 +22,22 @@ class FlatIterator:
             self._pop_list()
             return self.__next__()
 
- def test_1():
+def test_1():
 
-     list_of_lists_1 = [
-         ['a', 'b', 'c'],
-         ['d', 'e', 'f', 'h', False],
-         [1, 2, None]
-     ]
+ list_of_lists_1 = [
+     ['a', 'b', 'c'],
+     ['d', 'e', 'f', 'h', False],
+     [1, 2, None]
+ ]
 
-     for flat_iterator_item, check_item in zip(
-             FlatIterator(list_of_lists_1),
-             ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
-     ):
+ for flat_iterator_item, check_item in zip(
+         FlatIterator(list_of_lists_1),
+         ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+ ):
 
-         assert flat_iterator_item == check_item
+     assert flat_iterator_item == check_item
 
-     assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+ assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
 
 
 if __name__ == '__main__':
